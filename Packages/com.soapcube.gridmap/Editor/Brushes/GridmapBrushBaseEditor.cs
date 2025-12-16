521870b0c7dc630fb6c77388df83af652697f6e5
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-12-16 17:32:37 -0600

    Automatic brush elevation var

--- Packages/com.soapcube.gridmap/Editor/Brushes/GridmapBrushBaseEditor.cs
+++ Packages/com.soapcube.gridmap/Editor/Brushes/GridmapBrushBaseEditor.cs
@@ -42,15 +42,16 @@ namespace Gridmap.Brushes.Editor
             }
         }
 
-    
+
         // SerializedProperty variables.
+        private SerializedProperty brushElevation;
 
         /// <summary>
         /// Initialize SerializedProperties here.
         /// </summary>
         protected virtual void OnEnable()
         {
-
+            brushElevation = serializedObject.FindProperty(nameof(brushElevation));
         }
 
         /// <summary>
@@ -58,7 +59,64 @@ namespace Gridmap.Brushes.Editor
         /// </summary>
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
+            //base.OnInspectorGUI();
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(brushElevation);
+
+            // Update the position of the tilemap based on the brushElevation.
+            
+            // Add a space to separate custom data from the default brush properties.
+            EditorGUILayout.Space();
+            serializedObject.ApplyModifiedProperties();
         }
+
+        #region Elevation Adjusting
+        /// <summary>
+        /// Adjusts the elevation of the tilemap based on the brush's elevation and the swizzle mode of the grid.
+        /// </summary>
+        /// <param name="tilemapTarget">The target tilemap gameOjbect to adjust the position of.</param>
+        /// <param name="brushElevation">The current elevation set by the brush.</param>
+        /// <param name="swizzleMode">The swizzleMode of the grid.  Used to determine which axis to offset the grid by.</param>
+        private static void SetTilemapElevation(GameObject tilemapTarget, int brushElevation, Grid.CellSwizzle swizzleMode)
+        {
+            // Adjust the elevation of the brushTarget tilemap.
+            Vector3 pos;
+            switch (swizzleMode)
+            {
+                case GridLayout.CellSwizzle.XYZ:
+                case GridLayout.CellSwizzle.YXZ:
+                default:
+                    pos = Vector3.forward;
+                    break;
+                case GridLayout.CellSwizzle.XZY:
+                case GridLayout.CellSwizzle.YZX:
+                    pos = Vector3.up;
+                    break;
+                case GridLayout.CellSwizzle.ZXY:
+                case GridLayout.CellSwizzle.ZYX:
+                    pos = Vector3.right;
+                    break;
+            }
+            pos *= brushElevation;
+            tilemapTarget.transform.position = pos;
+        }
+
+        /// <summary>
+        /// On painting the scene GUI, adjust the elevation of the tilemap brushTarget based on the brushElevation.
+        /// </summary>
+        /// <param name="gridLayout"></param>
+        /// <param name="brushTarget"></param>
+        /// <param name="position"></param>
+        /// <param name="tool"></param>
+        /// <param name="executing"></param>
+        public override void OnPaintSceneGUI(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, 
+            GridBrushBase.Tool tool, bool executing)
+        {
+            SetTilemapElevation(brushTarget, targetBrush.BrushElevation, gridLayout.cellSwizzle);
+
+            base.OnPaintSceneGUI(gridLayout, brushTarget, position, tool, executing);
+        }
+        #endregion
     }
 }
