04410fb8003d7fdf9b20daabdd30974adfab3851
LucasFehlberg <lucas.fehlberg.17@gmail.com>
2025-12-21 13:19:18 -0600

    We can now attatch this to gameobjects (unity doesn't like it when we inherit directly from component) and we can place things in chunks

--- Packages/com.soapcube.gridmap/Runtime/Components/Gridmap.cs
+++ Packages/com.soapcube.gridmap/Runtime/Components/Gridmap.cs
@@ -1,18 +1,57 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 namespace Gridmap
 {
-    public class Gridmap : Component
+    public class Gridmap : MonoBehaviour
     {
+        /// <summary>
+        /// Size of the chunks
+        /// </summary>
         private readonly Vector3Int chunkSize = new(16, 16, 16);
 
-        private List<MeshChunk> chunks = new List<MeshChunk>();
+        /// <summary>
+        /// The chunks in this map
+        /// </summary>
+        private List<MeshChunk> chunks = new();
 
+        /// <summary>
+        /// Places a tile at this point, generating a new chunk if there isn't one already
+        /// </summary>
+        /// <param name="tile">The tile going into the chunk</param>
+        /// <param name="point">The point in grid space where the tile is going</param>
         public void PlaceTileAtPoint(MeshTileBase tile, Vector3Int point)
         {
+            Vector3Int chunkPosition = point;
 
+            //Suprisingly, you can get the index of a Vector3
+            //This function floors this to the position of Tile0 in a chunk
+            for (int index = 0; index < 3; index++)
+            {
+                float thingToFloor = chunkPosition[index] /= chunkSize[index];
+                chunkPosition[index] = Mathf.FloorToInt(thingToFloor) * chunkSize[index];
+            }
+
+            MeshChunk chunk = GetChunkByPosition(chunkPosition);
+            //If the chunk is null, we'll simply make a new chunk and add it to the list
+            if (chunk == null)
+            {
+                chunk = new(chunkPosition, chunkSize);
+                chunks.Add(chunk);
+            }
+
+            //Add the tile to the chunk
+            chunk.SetTileInChunk(tile, point);
+        }
+
+        /// <summary>
+        /// Gets the chunk at this position
+        /// </summary>
+        /// <param name="position">The position of the chunk</param>
+        /// <returns>The requested MeshChunk</returns>
+        private MeshChunk GetChunkByPosition(Vector3Int position)
+        {
+            return chunks.Find(c => c.Position == position);
         }
     }
 }
