ca2020888a1637276d9b3736cc513117de019e47
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-04-13 11:26:32 -0500

    Starting on elevation shortcuts

---
+++ Packages/com.soapcube.gridmap/Editor/GridmapShortcuts.cs
@@ -0,0 +1,27 @@
+/*****************************************************************************
+// File Name : GridmapShortcuts.cs
+// Author : Brandon Koederitz
+// Creation Date : 4/12/2025
+// Last Modified : 4/12/2025
+//
+// Brief Description : Handles all gridmap editor shortcuts.
+*****************************************************************************/
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+namespace Gridmap.Editor
+{
+    public static class GridmapShortcuts
+    {
+        [Shortcut("Gridmap/Gridmap Painter Up")]
+        public static void GridmapPainterUp()
+        {
+
+        }
+
+        public static void GridmapPainterDown()
+        {
+
+        }
+    }
+}
