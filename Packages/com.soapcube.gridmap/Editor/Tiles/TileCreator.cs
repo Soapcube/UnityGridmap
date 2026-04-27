e768a7230ed3f6c6f3ead099f1546bc8489d290d
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-04-26 19:40:56 -0500

    Just some small stuff

---
+++ Packages/com.soapcube.gridmap/Editor/Tiles/TileCreator.cs
@@ -0,0 +1,17 @@
+/*****************************************************************************
+// File Name : TileCreator.cs
+// Author : Arcadia Koederitz
+// Creation Date : 4/26/2025
+// Last Modified : 4/26/2025
+//
+// Brief Description : Handles auto-generating MeshTiles from user meshes.
+*****************************************************************************/
+using UnityEngine;
+
+namespace Gridmap.Editor
+{
+    public static class TileCreator
+    {
+    
+    }
+}
